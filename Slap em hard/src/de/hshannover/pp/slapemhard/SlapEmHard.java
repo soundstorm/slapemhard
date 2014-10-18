b41fb7fafb4f7148c3100a16eaa8de776a6488f1
Luca Zimmermann <info@djsoundstorm.de>
2014-10-18 22:55:47 +0200

    Quit listener

--- Slap em hard/src/de/hshannover/pp/slapemhard/SlapEmHard.java
+++ Slap em hard/src/de/hshannover/pp/slapemhard/SlapEmHard.java
@@ -1,12 +1,44 @@
 package de.hshannover.pp.slapemhard;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class SlapEmHard {
+	protected static JFrame frame;
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Slap Em Hard");
-		frame.setSize(800,600);
+		frame = new JFrame("Slap Em Hard");
+		frame.setSize(800, 600);
 		frame.setVisible(true);
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				System.out.println("ALT+F4/CMD+Q Fired");
+			}
+		});
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				confirmQuit();
+			}
+		});
+	}
+	
+	private static void confirmQuit() {
+		Object[] options = { "Beenden", "Abbrechen" };
+		int n = JOptionPane
+				.showOptionDialog(null,
+						"Möchtest du das Spiel wirklich beenden?",
+						"Spiel beenden", JOptionPane.OK_CANCEL_OPTION,
+						JOptionPane.WARNING_MESSAGE,
+						null,
+						options,
+						options[1]);
+		if (n == JOptionPane.OK_OPTION) {
+			System.exit(0);
+		}
+	}
+	
+	private void bla(){
+		
 	}
 }
