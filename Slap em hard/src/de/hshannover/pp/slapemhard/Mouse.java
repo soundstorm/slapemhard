8b85535d465e07f0705bd7eb454bfab5a4a2526f
Luca Zimmermann <info@djsoundstorm.de>
2014-10-18 23:19:39 +0200

    Initial commit

---
+++ Slap em hard/src/de/hshannover/pp/slapemhard/Mouse.java
@@ -0,0 +1,39 @@
+package de.hshannover.pp.slapemhard;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+//import java.awt.event.MouseAdapter;
+
+public class Mouse implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		//Fire
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
