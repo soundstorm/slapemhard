1ba7a3e4dc9e6d1ec83b610f21d0b58cfc93dcbc
Luca Zimmermann <info@djsoundstorm.de>
2014-10-22 18:19:18 +0200

    Changed from Me to Player

---
+++ Slap em hard/src/de/hshannover/pp/slapemhard/Player.java
@@ -0,0 +1,12 @@
+package de.hshannover.pp.slapemhard;
+
+import java.awt.Rectangle;
+
+public class Player extends Person {
+
+	public Player(int health) {
+		super(health,new Rectangle(0,0,10,30));
+		// TODO Auto-generated constructor stub
+	}
+	
+}
