7482611a199ae2b85f7a650174df2e0c6f0dd979
Luca Zimmermann <info@djsoundstorm.de>
2014-10-24 00:40:33 +0200

    Unnecessary, as all relevant should be in Person - all specific functions of Player should override methods of Person (movement, etc.)

--- Slap em hard/src/de/hshannover/pp/slapemhard/objects/Enemy.java
+++
@@ -1,9 +0,0 @@
-package de.hshannover.pp.slapemhard.objects;
-
-import java.awt.Rectangle;
-
-public class Enemy extends Person {
-	public Enemy(Rectangle size, Weapon weapon){
-		super(1,size);
-	}
-}
