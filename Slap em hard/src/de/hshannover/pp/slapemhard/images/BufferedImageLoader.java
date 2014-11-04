7ca272a979340e7fd005112333f7314e3ee02be6
Luca Zimmermann <info@djsoundstorm.de>
2014-11-04 11:08:31 +0100

    Initial Commit

---
+++ Slap em hard/src/de/hshannover/pp/slapemhard/images/BufferedImageLoader.java
@@ -0,0 +1,20 @@
+package de.hshannover.pp.slapemhard.images;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class BufferedImageLoader {
+	public BufferedImage getImage(String relPath) {
+		try {
+			File f = new File(relPath);
+			BufferedImage bi = ImageIO.read(f);
+			return bi;
+		} catch (IOException e) {
+			System.out.println("NULL");
+			return null;
+		}
+	}
+}
---
+++ Slap em hard/src/de/hshannover/pp/slapemhard/images/SpriteSheet.java
@@ -0,0 +1,58 @@
+package de.hshannover.pp.slapemhard.images;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * 
+ * @author SoundStorm
+ */
+
+public class SpriteSheet {
+	private BufferedImage bi;
+	private int tileWidth, tileHeight, cols, rows;
+	
+	/**
+	 * 
+	 * @param bi BufferedImage, representing the SpriteSheet
+	 * @param tileWidth Width of each tile
+	 * @param tileHeight Height of each tile
+	 */
+	public SpriteSheet(BufferedImage bi, int tileWidth, int tileHeight) {
+		this.bi = bi;
+		this.tileWidth = tileWidth;
+		this.tileHeight = tileHeight;
+		cols = bi.getWidth()/tileWidth;
+		rows = bi.getHeight()/tileHeight;
+	}
+	
+	/**
+	 * 
+	 * @param tile Number of the tile in SpriteSheet, numbered from left to right, top to bottom. 0-Indexed.
+	 * @return The requested tile. Null if tile exceeds the number of tiles.
+	 */
+	public BufferedImage getTile(int tile) {
+		/*if (tile >= cols*rows) {
+			return null;
+		}*/
+		return getTile(tile/cols, tile%cols);
+	}
+	/**
+	 * 
+	 * @param y
+	 * @param x 
+	 * @return
+	 */
+	public BufferedImage getTile(int y, int x) {
+		if ((x > cols)|(y > rows)) {
+			System.out.println("NULL");
+			return null;
+		}
+		return bi.getSubimage(tileWidth*x, tileHeight*y, tileWidth, tileHeight);
+	}
+	public int getWidth() {
+		return tileWidth;
+	}
+	public int getHeight() {
+		return tileHeight;
+	}
+}
