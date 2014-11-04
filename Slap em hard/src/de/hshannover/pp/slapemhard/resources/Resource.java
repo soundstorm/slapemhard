d0ee32a57b81d288a72e9758c359dbd768a367e4
Luca Zimmermann <info@djsoundstorm.de>
2014-11-04 15:08:10 +0100

    Mod

--- Slap em hard/src/de/hshannover/pp/slapemhard/resources/Resource.java
+++ Slap em hard/src/de/hshannover/pp/slapemhard/resources/Resource.java
@@ -7,7 +7,15 @@ import java.io.InputStream;
 
 public class Resource {
 	public InputStream getInputStream(String relativePath) {
-		System.out.println(this.getClass().getResource((">res>"+relativePath).replace(">", File.separator)).toString());
-		return this.getClass().getResourceAsStream(("/res>"+relativePath).replace(">", "/"));//File.separator));
+		try {
+			System.out.println(this.getClass().getResource((">res>"+relativePath).replace(">", File.separator)).toString());
+		} catch (NullPointerException e) {
+			try {
+				System.out.println(this.getClass().getResource((">res>"+relativePath).replace(">", "/")).toString());
+			} catch (NullPointerException i) {
+				System.out.println("NULL");
+			}
+		}
+		return this.getClass().getResourceAsStream((">res>"+relativePath).replace(">", "/"));//File.separator));
 	}
 }
